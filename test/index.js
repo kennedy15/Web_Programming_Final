19e519e37f3dde3543e2f63faaeccc860c798a6d
Nathan Kennedy <nathankennedy122@gmail.com>
2020-04-25 14:02:41 -0400

    Moved from mapbox to Leaflet
    This switch allows us to color each country with a different color depending on the statistic we want to show.

--- test/index.js
+++ test/index.js
@@ -6,51 +6,14 @@ function Country(name, death, recovered, confirmed) {
 }
 
 var countryArray = []; //array of country objects
-
-function init() {
-    get_data();
-
-    console.log(countryArray);
-
-    mapboxgl.accessToken = 'pk.eyJ1Ijoia2VubmVkeTE1IiwiYSI6ImNqdWlnZmNrNTFhNWo0M3Vqd2s1d3M4dWkifQ.kXdDtWHyleWospgGzCmwIg';
-    var map = new mapboxgl.Map({
-        container: 'map_area',
-        style: 'mapbox://styles/mapbox/dark-v10',
-        center: [4.899, 52.372],
-        zoom: 2,
-    });
-
-    map.scrollZoom.disable();
-
-
-    map.on('load', function() {
-            map.addSource('world_countries', {
-                type: 'geojson',
-                data: 'world.geojson'
-            });
-
-            map.addLayer({
-                'id': 'world_countries_fill',
-                'type': 'fill',
-                'source': 'world_countries',
-                'paint': {
-                    'fill-color': '#f08',
-                    'fill-opacity': 0.4
-                }
-            });
-    });
-
-
-}
-
 function getColorDeaths(d) {
-    return d > 1000 ? '#800026' :
-            d > 500  ? '#BD0026' :
-            d > 200  ? '#E31A1C' :
-            d > 100  ? '#FC4E2A' :
-            d > 50   ? '#FD8D3C' :
-            d > 20   ? '#FEB24C' :
-            d > 10   ? '#FED976' :
+    return d > 500000 ? '#800026' :
+            d > 350000  ? '#BD0026' :
+            d > 150000  ? '#E31A1C' :
+            d > 85000  ? '#FC4E2A' :
+            d > 25000   ? '#FD8D3C' :
+            d > 8500   ? '#FEB24C' :
+            d > 950   ? '#FED976' :
                         '#FFEDA0';
 }
 
@@ -73,6 +36,7 @@ function load_country_data(data) {
 
     for(let i = 0; i < json_Length; i++) {
         let tmp = new Country(); // create new Country object for each country
+        //let tmp = [];
         let name = c_keys[i] //get the name of the country
         let length = data[name].length; //get the length from inside the country
 
@@ -98,8 +62,143 @@ function load_country_data(data) {
         tmp.confirmed = tmp_confirmed;
         tmp.recovered = tmp_recovered;
 
+
+        //make an array of arrays instead
+        //tmp.push(name, tmp_deaths, tmp_confirmed, tmp_recovered);
+
         countryArray.push(tmp); //add the new object to an array to keep track
     }
+
+    //countryArray.forEach(state => console.log(state.death)); //debugging
+    //console.log(countryArray[0].death);
+    //console.log(countryArray[4].death);
+}
+
+function load_bar_chart(data) {
+    let w = 1400;
+    let h = 800;
+
+    let color = 'steelblue'
+
+    var chart = d3.select('#graph_area').append('svg')
+            .attr('width', w)
+            .attr('height', h)
+            .selectAll('rect')
+                .data(data)
+                .enter().append('rect')
+                    .style('fill', color)
+                    .attr('width', 15) //15 = barWidth
+                    .attr('height', function(d, i){
+                        return d[i][1];
+                    })
+                    .attr('x', function(i){
+                        return i * (15 + 5); //barwidth + barOffset
+                    })
+                    .attr('y', function(d, i){
+                        return h - d[i][1];
+                    })
+
+}
+
+function load_circular_bar_chart(data) {
+
+    //intialize variables
+    var margin = {top: 10, right: 10, bottom: 10, left: 10},
+        w = 900 - margin.left - margin.right,
+        h = 900 - margin.top - margin.bottom,
+        innerRadius = 100,
+        outerRadius = Math.min(w, h) / 2,
+        max = 100000,
+        color = 'steelblue';
+
+    //appending svg to the page
+    var svg = d3.select('#graph_area')
+        .append('svg')
+            .attr('width', w + margin.left + margin.right)
+            .attr('height', h + margin.top + margin.bottom)
+        .append('g')
+            .attr('transform', 'translate(' + w / 2 + ',' + (h / 2 + 100)+ ')');
+
+    // X scale
+    var x = d3.scaleBand()
+        .range([0,2 * Math.PI])
+        .align(0)
+        .domain(data.map(function(d, i) {
+            return d[i].death;
+        }) );
+
+    // Y scale
+    var y = d3.scaleRadial()
+        .range([innerRadius, outerRadius])
+        .domain([0, max]);
+
+    svg.append('g')
+        .selectAll('path')
+        .data(data)
+        .enter()
+        .append('path')
+            .attr('fill', color)
+            .attr('d', d3.arc()
+                .innerRadius(function(d, i) {
+                    return y(d[i].death);
+                })
+                .outerRadius(function(d, i) {
+                    return x(d[i].name);
+                })
+                .endAngle(function(d, i) {
+                    return x(d[i].name) + x.bandwidth();
+                })
+                .padAngle(0.01)
+                .padRadius(innerRadius))
+}
+
+function load_map() {
+    var mymap = L.map('map_area').setView([24.505, -0.09], 3);
+
+    //create the map
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/dark-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoia2VubmVkeTE1IiwiYSI6ImNqdWlnZmNrNTFhNWo0M3Vqd2s1d3M4dWkifQ.kXdDtWHyleWospgGzCmwIg'
+    }).addTo(mymap);
+
+    $.getJSON("countries.geojson",function(data){
+        // add GeoJSON layer to the map once the file is loaded
+        var datalayer = L.geoJson(data ,{
+            onEachFeature: function(feature, featureLayer) {
+                //set the name from the feature to a variable to use
+                let name = feature.properties.ADMIN;
+                //bind the name to a popup we can also bind the statistics to this later
+                featureLayer.bindPopup(name);
+                //find the data for the country in the countryArray
+                for(let i = 0; i < countryArray.length; i++) {
+                    if(name = countryArray[i].name) {
+                        //set the color based on the total number of deaths / recovered / confirmed-cases
+                        featureLayer.setStyle({fillColor : getColorDeaths(countryArray[i].death)})
+                    }
+                }
+            }
+        }).addTo(mymap);
+        mymap.fitBounds(datalayer.getBounds());
+    });
+}
+
+function init() {
+    get_data();
+    console.log(countryArray);
+
+    load_circular_bar_chart(countryArray);
+
+    //console.log(countryArray);
+
+    //load_bar_chart(countryArray);
+
+    //loading map
+    load_map();
+
 }
 
 
