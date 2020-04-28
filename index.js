0ca11d7b46ddef2699d3b4d6376a535ddbefe648
Justice Suh <justice.suh@gmail.com>
2020-04-28 10:51:30 -0400

    Create popup

--- index.js
+++ index.js
@@ -3,9 +3,6 @@
 
     function init() {
         getData();
-
-        //loading map for section1
-        // loadMap();
         createMap();
 
         //promise to load inthe data first then execute the visualization function for section2
@@ -16,14 +13,6 @@
         document.getElementById('deaths').addEventListener('click', () => {toggleMap('deaths')});
         document.getElementById('confirmed').addEventListener('click', () => {toggleMap('confirmed')});
         document.getElementById('recovered').addEventListener('click', () => {toggleMap('recovered')});
-    }
-
-    //Country Object
-    function Country(name, death, recovered, confirmed) {
-        this.name = name;
-        this.death = death;
-        this.recovered = recovered;
-        this.confirmed = confirmed;
     }
 
     let countries = {};
@@ -139,6 +128,30 @@
                 }
             });
         });
+
+        mymap.on('click', 'countries-fill', function(d) {
+            let countryName = d.features[0].properties['ADMIN'];
+            if (countryName in countries) {
+                let country = countries[countryName];
+                let html = countryName + '<br>' +
+                'Total Deaths: ' + country.deaths + '<br>' +
+                'Total Recovered: ' + country.recovered + '<br>' +
+                'Confrimed Cases: ' + country.confirmed;
+
+                new mapboxgl.Popup()
+                    .setLngLat(d.lngLat)
+                    .setHTML(html)
+                    .addTo(mymap);
+            }
+        });
+
+        mymap.on('mouseenter', 'countries-fill', function() {
+            mymap.getCanvas().style.cursor = 'pointer';
+        });
+        
+        mymap.on('mouseleave', 'countries-fill', function() {
+            mymap.getCanvas().style.cursor = '';
+        });
     }
 
     function toggleMap(stat) {
@@ -165,78 +178,6 @@
             }
         });
         mymap.getSource('countries-source').setData(countryData);
-    }
-
-    function loadMap() {
-        //by default the map will load based on the number of Deaths
-        mymap = L.map('map_area', {
-            center: [24.505, -0.09],
-            zoom: 5 //this is not work for setting the zoom level :(
-        });
-
-        //create the map
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 8,
-            id: 'mapbox/dark-v10',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'pk.eyJ1Ijoia2VubmVkeTE1IiwiYSI6ImNqdWlnZmNrNTFhNWo0M3Vqd2s1d3M4dWkifQ.kXdDtWHyleWospgGzCmwIg'
-        }).addTo(mymap);
-
-        loadMapData('death', mymap)
-    }
-
-    function loadMapData(stat, mymap) {
-        var stat = stat;
-
-        //checks if there is already a datalayer added to the map
-        if(mymap.hasLayer()) {
-            //removes it if so
-            mymap.removeLayer(datalayer);
-        }
-
-        //generates a new datalayer
-        $.getJSON("countries.geojson",function(data){
-            // add GeoJSON layer to the map once the file is loaded
-            let color = '';
-            let length = countryArray.length;
-            //console.log(length);
-            var datalayer = L.geoJson(data ,{
-                onEachFeature: function(feature, featureLayer) {
-                    //set the name from the feature to a variable to use
-                    let name = feature.properties.ADMIN;
-
-                    //find the data for the country in the countryArray
-                    for(let i = 0; i < length; i++) {
-                        if(name == countryArray[i].name) {
-                            //console.log(countryArray[i].death)
-                            //set the color based on the total number of deaths / recovered / confirmed-cases
-                            if(stat == 'recovered') {
-                                color = recoveredScale(countryArray[i].recovered);
-                                featureLayer.setStyle({fillColor : color});
-                                //binding statistics to the country as a popup!
-                                featureLayer.bindPopup('Name: ' + countryArray[i].name + '<br>' + 'total Deaths: ' + countryArray[i].death + '<br>' + 'total Recovered: ' + countryArray[i].recovered + '<br>' + 'total Confirmed Cases: ' + countryArray[i].confirmed);
-                            }
-                            else if(stat == 'confirmed') {
-                                color = confirmedScale(countryArray[i].confirmed);
-                                featureLayer.setStyle({fillColor : color});
-                                //binding statistics to the country as a popup!
-                                featureLayer.bindPopup('Name: ' + countryArray[i].name + '<br>' + 'total Deaths: ' + countryArray[i].death + '<br>' + 'total Recovered: ' + countryArray[i].recovered + '<br>' + 'total Confirmed Cases: ' + countryArray[i].confirmed);
-                            }
-                            else {
-                                color = deathScale(countryArray[i].death);
-                                featureLayer.setStyle({fillColor : color});
-                                //binding statistics to the country as a popup!
-                                featureLayer.bindPopup('Name: ' + countryArray[i].name + '<br>' + 'total Deaths: ' + countryArray[i].death + '<br>' + 'total Recovered: ' + countryArray[i].recovered + '<br>' + 'total Confirmed Cases: ' + countryArray[i].confirmed);
-                            }
-
-                        }
-                    }
-                }
-            }).addTo(mymap);
-            mymap.fitBounds(datalayer.getBounds());
-        });
     }
 
     function lineChart(data) {
